aa4c96c17a211debf01371373907a4eab960babf
Kevin Guran <kevin@gruzzlymug.com>
2018-01-17 00:59:39 -0600

    Land knights on mars (#1)

--- player-xpyre/run.py
+++ player-xpyre/run.py
@@ -3,10 +3,133 @@ import random
 import sys
 import traceback
 
-# unit type -> enum
-# worker 0, knight 1, factory 5
+#===============================================================================
 
-print("pystarting")
+# XPYRE Player
+
+#-------------------------------------------------------------------------------
+def do_factory(unit):
+    # early out if not done building
+    if not unit.structure_is_built():
+        return False
+
+    garrison = unit.structure_garrison()
+    if len(garrison) > 0:
+        d = random.choice(directions)
+        if gc.can_unload(unit.id, d):
+            # print('unloaded a thing!')
+            gc.unload(unit.id, d)
+            return True
+
+    need_knights = not bc.UnitType.Knight in last_counts or last_counts[bc.UnitType.Knight] < 50
+    if need_knights and gc.can_produce_robot(unit.id, bc.UnitType.Knight):
+        gc.produce_robot(unit.id, bc.UnitType.Knight)
+        # print('produced a knight!')
+        return True
+
+    need_healers = not bc.UnitType.Healer in last_counts or last_counts[bc.UnitType.Healer] < 2
+    if need_healers and gc.can_produce_robot(unit.id, bc.UnitType.Healer):
+        gc.produce_robot(unit.id, bc.UnitType.Healer)
+        # print('produced a healer!')
+        return True
+
+    return False
+
+#-------------------------------------------------------------------------------
+def do_worker(unit):
+    dd = random.choice(directions)
+
+    need_workers = not bc.UnitType.Worker in last_counts or last_counts[bc.UnitType.Worker] < 4
+    if need_workers and gc.can_replicate(unit.id, dd):
+        gc.replicate(unit.id, dd)
+        return True
+
+    need_rockets = not bc.UnitType.Rocket in last_counts or last_counts[bc.UnitType.Rocket] < 4
+    if need_rockets and gc.can_blueprint(unit.id, bc.UnitType.Rocket, dd):
+        # print("BUILT A ROCKET")
+        gc.blueprint(unit.id, bc.UnitType.Rocket, dd)
+        return True
+
+    # next, let's look for nearby blueprints to work on
+    nearby = gc.sense_nearby_units(unit.location.map_location(), 2)
+    for other in nearby:
+        if gc.can_build(unit.id, other.id):
+            gc.build(unit.id, other.id)
+            # print('built something:')
+            return True
+    return False
+
+#-------------------------------------------------------------------------------
+def do_knight(unit):
+    # print("knight")
+    nearby = gc.sense_nearby_units(unit.location.map_location(), 2)
+    for other in nearby:
+        # if other.team != my_team and gc.is_attack_ready(unit.id) and gc.can_attack(unit.id, other.id):
+        #     # print('attacked a thing!')
+        #     gc.attack(unit.id, other.id)
+        #     return True
+        if other.unit_type == bc.UnitType.Rocket and gc.can_load(other.id, unit.id):
+            if not other.rocket_is_used():
+                # print("knight boarding rocket")
+                gc.load(other.id, unit.id)
+                return True
+    return False
+
+#-------------------------------------------------------------------------------
+def find_landing_site():
+    dest = bc.MapLocation(bc.Planet.Mars, 0, 0)
+
+    no_landing_site = True
+    for n in range(7):
+        dest.x = random.randint(0, mars_map.width)
+        dest.y = random.randint(0, mars_map.height)
+        if no_landing_site and mars_map.is_passable_terrain_at(dest):
+            no_landing_site = False
+            # print("found dest", dest.x, dest.y)
+            return dest
+
+    # TODO return null or something...dest is not valid
+    if no_landing_site:
+        # print("nuts")
+        return dest
+
+#-------------------------------------------------------------------------------
+def do_rocket(unit):
+    # early out if not done building
+    if not unit.structure_is_built():
+        return False
+
+    if unit.rocket_is_used():
+        garrison = unit.structure_garrison()
+        if len(garrison) > 0:
+            d = random.choice(directions)
+            if gc.can_unload(unit.id, d):
+                # print('unloaded on mars!')
+                gc.unload(unit.id, d)
+                return True
+
+    # TODO this is a hack
+    if unit.rocket_is_used():
+        # nothing to unload, dead in the water
+        return False
+
+    garrison = unit.structure_garrison()
+    if len(garrison) == 0:
+        # print("no crew! aborting!")
+        return False
+
+    landing_site = find_landing_site()
+
+    # TODO this effectively crashes docker...why?
+    # if gc.can_launch_rocket(unit.id, landing_site)
+    print("launching!")
+    gc.launch_rocket(unit.id, landing_site)
+    #     return True
+    return False
+
+#===============================================================================
+
+print("XPYRE 2018.01.16c")
 
 # A GameController is the main type that you talk to the game with.
 # Its constructor will connect to a running game.
@@ -20,43 +143,16 @@ my_team = gc.team()
 # aside from turns taking slightly different amounts of time due to noise.
 random.seed(6137)
 
-has_rocket = False
+earth_map = gc.starting_map(bc.Planet.Earth)
+mars_map = gc.starting_map(bc.Planet.Mars)
+
+unit_counts = {}
+last_counts = {}
+
+# NOTE not used
+old_landing_sites = []
 
 print("pystarted")
-
-def do_factory(unit):
-    garrison = unit.structure_garrison()
-    if len(garrison) > 0:
-        d = random.choice(directions)
-        if gc.can_unload(unit.id, d):
-            print('unloaded a thing!')
-            gc.unload(unit.id, d)
-            return True
-    elif gc.can_produce_robot(unit.id, bc.UnitType.Knight):
-        gc.produce_robot(unit.id, bc.UnitType.Knight)
-        print('produced a knight!')
-        return True
-    elif gc.can_produce_robot(unit.id, bc.UnitType.Healer):
-        gc.produce_robot(unit.id, bc.UnitType.Healer)
-        print('produced a healer!')
-        return True
-    return False
-
-def do_worker(unit):
-    dd = random.choice(directions)
-    if gc.can_blueprint(unit.id, bc.UnitType.Rocket, dd):
-        # print("BUILT A ROCKET")
-        gc.blueprint(unit.id, bc.UnitType.Rocket, dd)
-        return True
-
-    # next, let's look for nearby blueprints to work on
-    nearby = gc.sense_nearby_units(unit.location.map_location(), 2)
-    for other in nearby:
-        if gc.can_build(unit.id, other.id):
-            gc.build(unit.id, other.id)
-            # print('built a factory!')
-            return True
-    return False
 
 # let's start off with some research!
 # we can queue as much as we want.
@@ -68,39 +164,42 @@ gc.queue_research(bc.UnitType.Healer)
 while True:
     # We only support Python 3, which means brackets around print()
     turn = gc.round()
-    if turn % 100 == 0:
-        print('pyround:', turn)
 
     # TODO "classify others" pre-step
+
+    last_counts = unit_counts
+    unit_counts = {}
 
     # frequent try/catches are a good idea
     try:
         # walk through our units:
         for unit in gc.my_units():
-            location = unit.location
-            if not location.is_on_map():
+            ut = unit.unit_type
+            if ut in unit_counts:
+                unit_counts[ut] += 1
+            else:
+                unit_counts[ut] = 1
+
+            if not unit.location.is_on_map():
                 # print("{} not on map".format(unit.unit_type))
                 continue
 
-            # first, factory logic
-            if unit.unit_type == bc.UnitType.Factory:
+            if ut == bc.UnitType.Factory:
                 handled = do_factory(unit)
                 if handled:
-                    # print(">> factory")
                     continue
-            elif unit.unit_type == bc.UnitType.Worker:
+            elif ut == bc.UnitType.Worker:
                 handled = do_worker(unit)
-                # if handled:
-                #     # move onto the next unit
-                #     continue
-            elif unit.unit_type == bc.UnitType.Knight:
-                # print("knight {}".format(unit.unit_type))
-                nearby = gc.sense_nearby_units(location.map_location(), 2)
-                for other in nearby:
-                    if other.team != my_team and gc.is_attack_ready(unit.id) and gc.can_attack(unit.id, other.id):
-                        print('attacked a thing!')
-                        gc.attack(unit.id, other.id)
-                        continue
+                if handled:
+                    continue
+            elif ut == bc.UnitType.Knight:
+                handled = do_knight(unit)
+                if handled:
+                    continue
+            elif ut == bc.UnitType.Rocket:
+                handled = do_rocket(unit)
+                if handled:
+                    continue
 
             # okay, there weren't any dudes around
             # pick a random direction:
